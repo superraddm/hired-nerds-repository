31c364a9dfbb380094d3efd88b25a61dd2bc836f
superraddm <joffad@gmail.com>
2025-12-18 11:01:56 +0000

    Laptop last updates

--- hirednerds-chat/app/src/index.js
+++ hirednerds-chat/app/src/index.js
@@ -2,10 +2,6 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-     // Serve static HTML files
-    if (url.pathname === '/download.html') {
-      return env.ASSETS.fetch(request);
-    }
     
     // CORS preflight
     if (request.method === "OPTIONS") {
